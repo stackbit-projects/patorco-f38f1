caf42d312473d8e4d3c9dd83ad06b854534edf12
Stackbit Code Editor <support@stackbit.com>
2021-03-03 08:58:10 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 
 import {toStyleObj, withPrefix, markdownify, Link, classNames} from '../utils';
 
-
+import data from '../'
 
 
 export default class HeroSection extends React.Component {
