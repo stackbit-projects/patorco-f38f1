1471a80ff007a89594c46e598433d7d95421e43c
Stackbit Code Editor <support@stackbit.com>
2021-03-03 08:19:18 +0000

    product.js: changed

--- src/templates/product.js
+++ src/templates/product.js
@@ -85,7 +85,15 @@ export default class Product extends React.Component {
                         see all products
                     </Link>
                 </main>
-                
+                 <main className={'content' + (_.get(this.props, 'pageContext.frontmatter.page_css_class', null) ? (' ' + _.get(this.props, 'pageContext.frontmatter.page_css_class', null)) : '')}>
+                {_.map(_.get(this.props, 'pageContext.frontmatter.sections', null), (section, section_idx) => {
+                    let component = _.upperFirst(_.camelCase(_.get(section, 'type', null)));
+                    let Component = components[component];
+                    return (
+                    <Component key={section_idx} {...this.props} section={section} page={this.props.pageContext} site={this.props.pageContext.site} />
+                    )
+                })}
+            </main>
             </Layout>
             
         );
