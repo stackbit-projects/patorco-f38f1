87364091bfc43bddfc24fda8b64a5e38d69e366f
Stackbit Code Editor <support@stackbit.com>
2021-03-03 08:47:45 +0000

    product.js: changed

--- src/templates/product.js
+++ src/templates/product.js
@@ -7,6 +7,7 @@ import {toStyleObj, withPrefix, Link, getPageByFilePath, getPages} from '../util
 import Picture from '../components/Picture';
 import BuyButton from '../components/BuyButton';
 import ProductGrid from '../components/ProductGrid';
+import HeroSection from '../components/'
 
 // this minimal GraphQL query ensures that when 'gatsby develop' is running,
 // any changes to content files are reflected in browser
